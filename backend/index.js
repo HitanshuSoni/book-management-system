4db523b08b1318d8acabab00e38e69bca66eb42b
HitanshuIntern <hitanshu.intern@freehitnews.com>
2024-01-05 21:57:26 +0530

    setup main file

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/routes");
+const multer = require("multer");
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://hitanshusoni10:charmendar123@bmscluster.yngdopl.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+app.use(express.json()); //bson
+app.use(multer().any());
+const port = 3000;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose
+  .connect(
+    "mongodb+srv://hitanshusoni10:charmendar123@bmscluster.yngdopl.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDB Running"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.use("/*",  (req, res) =>{
+  return res.status(400).send({ status: false, msg: "You Are In Wrong Path" });
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+app.listen(port, function () {
+  console.log("Express Running on " + port);
+});
