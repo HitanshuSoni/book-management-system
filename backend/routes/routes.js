885e7d9b2644bec78fdde209fb693368888b81d6
HitanshuIntern <hitanshu.intern@freehitnews.com>
2024-01-05 21:54:02 +0530

    routes

---
+++ backend/routes/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.post("/signup", userController.signup); //signup user
+
+router.post("/login", userController.login); //login user
