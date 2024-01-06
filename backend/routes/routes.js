be25496013dcaeb558cefaeb773f4b556f10030f
HitanshuIntern <hitanshu.intern@freehitnews.com>
2024-01-06 05:54:53 +0530

    add post route for publish book

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const bookController = require("../controllers/bookController");
+const middleware = require("../middleware/middleware");
+
 
 router.post("/signup", userController.signup); //signup user
 
 router.post("/login", userController.login); //login user
 
+router.post(
+    "/books/publish",
+    middleware.authentication,
+    middleware.authorization,
+    bookController.createBook
+  ); //create book
 
 module.exports = router;
