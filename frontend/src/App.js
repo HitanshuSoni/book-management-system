32cf5f6bb980452015fe70b63635e63c82950768
HitanshuIntern <hitanshu.intern@freehitnews.com>
2024-01-07 13:37:16 +0530

    add nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AllBooks from './components/AllBooks';
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
 import MyBooks from './components/MyBooks';
 import PublishBookForm from './components/PublishBookForm';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-function App() {
+const NavigationBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          BookApp
+        </Typography>
+        <Button color="inherit" component={Link} to="/">All Books</Button>
+        <Button color="inherit" component={Link} to="/my-books">My Books</Button>
+        <Button color="inherit" component={Link} to="/publish-book">Publish Book</Button>
+        <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+        <Button color="inherit" component={Link} to="/login">Log In</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+const App = () => {
   return (
     <Router>
       <div>
-
+        <NavigationBar />
         <Routes>
           <Route path="/" element={<AllBooks />} />
           <Route path="/signup" element={<SignUp />} />
@@ -18,10 +36,9 @@ function App() {
           <Route path="/my-books" element={<MyBooks />} />
           <Route path="/publish-book" element={<PublishBookForm />} />
         </Routes>
-
       </div>
     </Router>
   );
-}
+};
 
 export default App;
